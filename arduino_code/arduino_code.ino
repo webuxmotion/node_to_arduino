51d9efb2febda63a5360e25c0ba7095a0fe6c368
Andrii Pereverziev <pereverziev.andrii@gmail.com>
2022-08-06 22:32:45 +0300

    Read 2

--- arduino_code/arduino_code.ino
+++ arduino_code/arduino_code.ino
@@ -1,8 +1,17 @@
+#include <ArduinoJson.h>
+
+
 int lightPin = 2;
 
 // for processPotentiometer()
 int percent = 0;
 int prevPercent = 0;
+
+int prevLed = 0;
+
+DynamicJsonDocument doc(1024);
+
+String prevDoc;
 
 void setup() {
   pinMode(13, OUTPUT);
@@ -11,39 +20,58 @@ void setup() {
 }
 
 void loop() {
+  sendJson();
+  
   if (Serial.available() > 0) {
     char ReaderFromNode; // Store current character
     ReaderFromNode = (char) Serial.read();
     convertToState(ReaderFromNode); // Convert character to state  
-  }
- 
-  delay(100);
-
+  } 
+  
   processPotentiometer();
+
+  delay(100);
 }
 
 void convertToState(char chr) {
-  Serial.println(chr);
   
-  if(chr=='w'){
+  if(chr=='w') {
     digitalWrite(LED_BUILTIN, HIGH);
     digitalWrite(lightPin,HIGH);
-    delay(100); 
+    doc["led"] = HIGH;
+    delay(10);
   }
-  if(chr=='t'){
+  if(chr=='t') {
     digitalWrite(LED_BUILTIN, LOW);
     digitalWrite(lightPin,LOW);
-    delay(100); 
+    doc["led"] = LOW;
+    delay(10);
   }
+
+  delay(10);
 }
 
 void processPotentiometer() {
+  
   percent = round(analogRead(0) / 1024.00 * 100);
   
   if(percent != prevPercent) {
-    Serial.println(percent);
     prevPercent = percent;
+    doc["potentiometr"] = percent;
+    doc["led"] = digitalRead(LED_BUILTIN) || digitalRead(lightPin);
+    
+    delay(10);
   }
   
-  delay(100);
+  delay(10);
+}
+
+void sendJson() {
+  String newDoc = doc.as<String>();
+  
+  if (newDoc != prevDoc) {
+    Serial.println();
+    serializeJson(doc, Serial);
+    prevDoc = doc.as<String>();
+  }
 }
